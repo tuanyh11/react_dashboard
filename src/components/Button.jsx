1ddd3fea65c848cd4e3e18f75e10f7e29cd19fe1
tuan leo <you@example.com>
2022-11-02 09:05:49 +0700

    change

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,14 +1,22 @@
 import React from 'react';
+import {useNavigate} from "react-router-dom"
 
 import { useStateContext } from '../contexts/ContextProvider';
 
 const Button = ({ icon, bgColor, color, bgHoverColor, size, text, borderRadius, width }) => {
   const { setIsClicked, initialState } = useStateContext();
 
+  const nav = useNavigate()
+
+  const handleLogout = () => { 
+    localStorage.clear()
+    nav("/login")
+  }
+
   return (
     <button
-      type="button"
-      onClick={() => setIsClicked(initialState)}
+      type="button" 
+      onClick={() => handleLogout()}
       style={{ backgroundColor: bgColor, color, borderRadius }}
       className={` text-${size} p-3 w-${width} hover:drop-shadow-xl hover:bg-${bgHoverColor}`}
     >
